02ef6f32aaa92576f4b6fad057f79786b8d1d83b
JMarcosSF <jamlmarcos@gmail.com>
2016-10-28 23:44:00 -0700

    Updated exam-result-dialog.html UI/UX by replacing the selectable fields in the form group with a a table of checkboxes to provide a better UX/UI.

--- src/main/webapp/app/entities/exam-result/exam-result-dialog.controller.js
+++ src/main/webapp/app/entities/exam-result/exam-result-dialog.controller.js
@@ -11,8 +11,11 @@
         var vm = this;
 
         vm.examResult = entity;
+        vm.examResult.answers = [];
         vm.clear = clear;
         vm.save = save;
+        vm.filterAnswers = filterAnswers;
+        vm.checkboxFix = checkboxFix;
         Course.query().$promise
             .then(function(value) {
                 vm.courses = value;
@@ -20,6 +23,7 @@
         }, function(err) {
             console.log("Error: " + err);
         });
+
         vm.examResult.course = null;
         $scope.$watch('vm.examResult.course', function() {
             if(vm.examResult.course) {
@@ -59,6 +63,8 @@
 
         function save () {
             vm.isSaving = true;
+            vm.examResult.questions = vm.questions;
+            console.log(vm.examResult.answers + "!!!!!!!!!!!!");
             if (vm.examResult.id !== null) {
                 ExamResult.update(vm.examResult, onSaveSuccess, onSaveError);
             } else {
@@ -76,6 +82,36 @@
             vm.isSaving = false;
         }
 
+        function filterAnswers(answers, question) {
+            var ans = [];
+            if(answers !== undefined) {
+                for(var i = 0; i < answers.length; i++) {
+                    if(answers[i].question.id === question.id) {
+                        ans.push(answers[i]);
+                    }
+                }
+            }
+            return ans;
+        }
+
+        function checkboxFix() {
+            console.log("!!!!!HERE");
+            $("input:checkbox").on('click', function() {
+                // in the handler, 'this' refers to the box clicked on
+                var $box = $(this);
+                if ($box.is(":checked")) {
+                    // the name of the box is retrieved using the .attr() method
+                    // as it is assumed and expected to be immutable
+                    var group = "input:checkbox[name='" + $box.attr("name") + "']";
+                    // the checked state of the group/box on the other hand will change
+                    // and the current value is retrieved using .prop() method
+                    $(group).prop("checked", false);
+                    $box.prop("checked", true);
+                } else {
+                    $box.prop("checked", false);
+                }
+            });
+        }
 
     }
 })();
